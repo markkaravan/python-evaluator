9838651861add69ff0da45a1a63a3d2fbdac6e7b
Mark Karavan <mkaravan@Marks-MacBook-Pro.local>
2021-12-29 00:15:29 -0500

    builds ast

--- parser.py
+++ parser.py
@@ -4,19 +4,30 @@ class Parser():
 
     def parse(self, input):
         tokens = self.tokenize(input)
-        # ast = self.generateAst(tokens, [])
-        return tokens
+        ast = self.generateAst(tokens)
+        return ast
 
     def tokenize(self, s):
         tokens = s.replace('(', ' ( ').replace(')', ' ) ').split(' ')
         cleaned = [t for t in tokens if t != '']
         return cleaned
 
-    # def generateAst(self, tokens, ast):
-    #     for token in tokens:
-
-
-
+    def generateAst(self, tokens):
+        stack = []
+        for token in tokens:
+            if token == '(':
+                stack.append(token)
+            elif token == ')':
+                branch = []
+                while stack[-1] != '(':
+                    branch.insert(0, stack.pop())
+                stack.pop() # final '('
+                stack.append(branch)
+            elif token.isnumeric():
+                stack.append(float(token))
+            else:
+                stack.append(token)
+        return stack
 
 
 p = Parser()
